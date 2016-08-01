5e71a4908c8b7aea8d01a8d9ae7ac41518898aed
Меркулов Сергей Михайлович <merkulov.s.m@gmail.com>
2016-08-01 18:30:26 +0400

    Готовая

--- js/main.js
+++ js/main.js
@@ -15,14 +15,34 @@
 			countOfHouses: 34
 		}
 	]
-	var form, buttonAdd, buttonDelete, i, arDeleteButtons;
-
-	function showAllElements(arObjects, $placeInDom) {
-		var compiled, html;
-
-		compiled = _.template('<table><% _.forEach(arObjects, function(key) { %><tr><td><%- key.id %></td><td><%- key.city %></td><td><%- key.street %></td><td><%- key.countOfHouses %></td><td><button id="<%- key.id %>" class="js-delete-element">Delete</button></td></tr><% }); %></table>');
+	var form, buttonAdd, buttonFullVersion, buttonCompactVersion, buttonFindMin, buttonFindMax;
+	
+	function reindexArray(arObjects) {
+		var arTmp = [];
+		var i;
+		for (i = 0; i < arObjects.length; i++) {
+			
+			if ( i in arObjects ) {
+				arTmp.push(arObjects[i]);
+			}
+		}
+		return arTmp;
+	}
+	function showAllElements(arObjects, placeInDom) {
+		var compiled, html;	
+		arObjects = reindexArray(arObjects);
+		compiled = _.template('<table><% _.forEach(arObjects, function(value, key) { %><tr><td><%- value.id %></td><td><%- value.city %></td><td><%- value.street %></td><td><%- value.countOfHouses %></td><td><button id="<%- key %>" class="js-delete-element">Delete</button></td></tr><% }); %></table>');
 		html = compiled({'arObjects': arObjects});
-		document.getElementById($placeInDom).innerHTML = html;
+		document.getElementById(placeInDom).innerHTML = html;
+		return arObjects;
+	}
+	function showAllElementsCompact(arObjects, placeInDom) {
+		var compiled, html;	
+		arObjects = reindexArray(arObjects);
+		compiled = _.template('<table><% _.forEach(arObjects, function(value, key) { %><tr><td><%- value.city %>, <%- value.street %> - <%- value.countOfHouses %></td><td><button id="<%- key %>" class="js-delete-element">Delete</button></td></tr><% }); %></table>');
+		html = compiled({'arObjects': arObjects});
+		document.getElementById(placeInDom).innerHTML = html;
+		return arObjects;
 	}
 	function addElement(arObjects) {
 		arObjects.push({
@@ -31,29 +51,71 @@
 			street: document.getElementById('js-street-field').value,
 			countOfHouses: document.getElementById('js-countOfHouses-field').value
 		});
-		showAllElements(arObjects, 'js-table');
+		arObjects = showAllElements(arObjects, 'js-table');
+		return arObjects;
 	}
-
 	function deleteElement(arObjects, elementID) {
-		delete arObjects[elementID-1];
-		console.log(arObjects);
-		showAllElements(arObjects, 'js-table');
+		delete arObjects[elementID];
+		arObjects = showAllElements(arObjects, 'js-table');
+		return arObjects;
 	}
-
+	function findMin(arObjects) {
+		var minCount = _.minBy(arObjects, function(o) { return o.countOfHouses; });
+		_.forEach(arObjects, function(value, key) {
+			if(minCount.countOfHouses == value.countOfHouses) {
+				document.getElementById(key).parentNode.parentNode.style.background = 'yellow';
+			}
+		});
+		return arObjects;
+	}
+	function findMax(arObjects) {
+		var maxCount = _.maxBy(arObjects, function(o) { return o.countOfHouses; });
+		_.forEach(arObjects, function(value, key) {
+			if(maxCount.countOfHouses == value.countOfHouses) {
+				document.getElementById(key).parentNode.parentNode.style.background = 'green';
+			}
+		});
+		return arObjects;
+	}
+	
+	/* Full version view */
+	buttonFullVersion = document.getElementById('js-full-version');
+	buttonFullVersion.addEventListener('click', function(event) {
+		arObjects = showAllElements(arObjects, 'js-table');
+		event.stopPropagation();
+	});
+	/* Compact version view */
+	buttonCompactVersion = document.getElementById('js-short-version');
+	buttonCompactVersion.addEventListener('click', function(event) {
+		arObjects = showAllElementsCompact(arObjects, 'js-table');
+		event.stopPropagation();
+	});
+	/* Find min count of houses */
+	buttonFindMin = document.getElementById('js-findmin-version');
+	buttonFindMin.addEventListener('click', function(event) {
+		arObjects = findMin(arObjects);
+		event.stopPropagation();
+	});
+	/* Find max count of houses */
+	buttonFindMax = document.getElementById('js-findmax-version');
+	buttonFindMax.addEventListener('click', function(event) {
+		arObjects = findMax(arObjects);
+		event.stopPropagation();
+	});
+	/* Add element */
 	buttonAdd = document.getElementById('js-add-button');
 	buttonAdd.addEventListener('click', function(event) {
-		addElement(arObjects);
+		arObjects = addElement(arObjects);
 		event.stopPropagation();
-		event.stopEvent();
 	});
-
-
+	/* Delete element */
 	form = document.getElementById('js-table');
 	form.addEventListener('click', function(event) {
 		if(event.target.tagName === 'BUTTON') {
-			deleteElement(arObjects, event.target.id)
+			arObjects = deleteElement(arObjects, event.target.id)
 		}
 	});
+	
 	showAllElements(arObjects, 'js-table');
 
 })();
